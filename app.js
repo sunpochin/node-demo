554945462afea9fbc09f6f669308a6ece443f165
sun pochin <sunpochin@gmail.com>
2021-05-30 22:33:21 +0800

    changing port for heroku.

--- app.js
+++ app.js
@@ -259,7 +259,7 @@ app.post("/login", function(req, res) {
   });
 });
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || "3000";
+app.listen(port, function() {
+  console.log("Server started on port: ", port);
 });
